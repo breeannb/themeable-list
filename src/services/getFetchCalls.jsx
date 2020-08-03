0eadbff08bba5db1700e95c47465402867739857
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-08-03 13:16:28 -0700

    added a fetch call to get all characters

--- src/services/getFetchCalls.jsx
+++ src/services/getFetchCalls.jsx
@@ -1,1 +1,4 @@
-console.log('hello world');
+export const getAvatarCharactersList = page => {
+  return fetch(`https://last-airbender-api.herokuapp.com/api/v1/characters?page=${page}`)
+    .then(res => res.json());
+};
